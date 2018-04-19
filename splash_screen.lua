9798ca54dcf64bb27594c5d845ce0f9a1ffade11
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-04-19 11:04:53 -0400

    ok

---
+++ splash_screen.lua
@@ -0,0 +1,14 @@
+
+-- Use Composer Library
+local composer = require( "composer" )
+
+-- Name the Scene
+sceneName = "splash_screen"
+
+-----------------------------------------------------------------------------------------
+
+-- Create Scene Object
+local scene = composer.newScene( sceneName )
+
+-- local variables
+local CompanyLogo = display.newImageRect(Images/)
