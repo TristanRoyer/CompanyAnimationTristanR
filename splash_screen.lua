9012dbb458c3bc0a15057524f26c0560d68bd274
TristanRoyer <Tristan.Royer@ocsbstudent.ca>
2018-04-22 23:42:20 -0400

    ok

--- splash_screen.lua
+++ splash_screen.lua
@@ -1,4 +1,14 @@
 
+
+-----------------------------------------------------------------------------------------
+--
+-- splash_screen.lua
+-- Created by: Your Name
+-- Date: Month Day, Year
+-- Description: This is the splash screen of the game. It displays the 
+-- company logo that...
+-----------------------------------------------------------------------------------------
+
 -- Use Composer Library
 local composer = require( "composer" )
 
@@ -10,5 +20,157 @@ sceneName = "splash_screen"
 -- Create Scene Object
 local scene = composer.newScene( sceneName )
 
--- local variables
-local CompanyLogo = display.newImageRect(Images/)
+----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+ 
+-- The local variables for this scene
+local CompanyLogo = display.newImageRect
+local DramaticSounds = audio.loadSound("Sounds/bensound-ukulele.mp3")
+local DramaticSoundsChannel
+CompanyLogo.alpha = 0
+local CompanyLogoText = display.newText("4 Cake Company",0,0,Arial,70)
+CompanyLogoText:setTextColor(1 , 0, 1)
+CompanyLogoText.alpha = 0
+
+--------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+--------------------------------------------------------------------------------------------
+
+--
+local function FadeIn()
+    CompanyLogo.alpha = CompanyLogo.alpha + 0.5
+    CompanyLogoText.alpha = CompanyLogoText.alpha + 0.5
+end
+ function FadeInEvent( event )
+    timer.performWithDelay(200,FadeIn)
+end	
+
+local function FadeOut()
+    CompanyLogo.alpha = CompanyLogo.alpha - 0.5
+end
+local function FadeOutEvent(event)
+timer.performWithDelay(1300,FadeOut)
+end
+
+-- The function that will go to the main menu 
+local function gotoMainMenu()
+    composer.gotoScene( "main_menu" )
+end
+
+-----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- The function called when the screen doesn't exist
+function scene:create( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -- set the background to be black
+    display.setDefault("background", 0, 0, 0)
+
+    -- Insert the beetleship image
+    CompanyLogo = display.newImageRect("Images/rocket.png", 200, 200)
+
+    -- set the initial x and y position of the beetleship
+    CompanyLogo.x = 100
+    CompanyLogo.y = display.contentHeight/2
+
+    -- Insert objects into the scene group in order to ONLY be associated with this scene
+    sceneGroup:insert( CompanyLogo )
+
+end -- function scene:create( event )
+
+--------------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to appear on screen
+function scene:show( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is still off screen (but is about to come on screen).
+    if ( phase == "will" ) then
+       
+    -----------------------------------------------------------------------------------------
+
+    elseif ( phase == "did" ) then
+        -- start the splash screen music
+        DramaticSoundsChannel = audio.play( DramaticSounds )
+        Runtime.addEventListener("enterFrame",FadeInEvent)
+        if (CompanyLogo.alpha = 1 ) then
+            Runtime.addEventListener("enterFrame",FadeOutEvent)
+        end
+
+         
+        -- Go to the main menu screen after the given time.
+        timer.performWithDelay ( 3000, gotoMainMenu)          
+        
+    end
+
+end --function scene:show( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to leave the screen
+function scene:hide( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is on screen (but is about to go off screen).
+    -- Insert code here to "pause" the scene.
+    -- Example: stop timers, stop animation, stop audio, etc.
+    if ( phase == "will" ) then  
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called immediately after scene goes off screen.
+    elseif ( phase == "did" ) then
+        
+        -- stop the jungle sounds channel for this screen
+        audio.stop(DramaticSoundsChannel)
+    end
+
+end --function scene:hide( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to be destroyed
+function scene:destroy( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+
+    -- Called prior to the removal of scene's view ("sceneGroup").
+    -- Insert code here to clean up the scene.
+    -- Example: remove display objects, save state, etc.
+end -- function scene:destroy( event )
+
+-----------------------------------------------------------------------------------------
+-- EVENT LISTENERS
+-----------------------------------------------------------------------------------------
+
+-- Adding Event Listeners
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
